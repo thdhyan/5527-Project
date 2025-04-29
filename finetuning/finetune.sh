b9202b7b78d7e275d74e18436a194a91247fce9f
Matt <rajal038@umn.edu>
2025-04-29 12:59:09 -0500

    new script

---
+++ finetuning/finetune.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Download images/JSON files:
+# TODO: make python script to download Google folder
+
+# Clone Qwen-VL
+git clone https://github.com/QwenLM/Qwen-VL.git
+
+# Deepspeed run
